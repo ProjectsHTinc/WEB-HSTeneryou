aa5171de7c75a57e87d2163c5b52cdad844c0673
senmaran <maran.happysanz@gmail.com>
2018-12-29 16:28:04 +0530

    graph font changes
    graph font changes

--- src/components/Graph_1.js
+++ src/components/Graph_1.js
@@ -2,10 +2,8 @@ import React, {Component} from "react";
 import Chart from "react-google-charts";
 
 class Graph_1 extends  Component{
-
     render(){
         const graph1_values = JSON.parse(localStorage.getItem('google_graph1'));
-       
         return(
             <div className="container">
             <div className="row">
@@ -14,24 +12,22 @@ class Graph_1 extends  Component{
             <div className="container">
             <Chart id='Graph 1'
                 width={'100%'}
-                height={'440px'}
+                height={'405px'}
                 chartType="ComboChart"
                 loader={<div>Loading Chart</div>}
                 data={graph1_values}
                  // Set chart options
                 options = {{
                     title : 'Comparison of Electrical Self Sufficiency',
+                    titleTextStyle: {color: '#ff0000',fontSize : 10},
+                    fontName: "Roboto",
+                    legendTextStyle: {color: '#ff0000',fontSize : 10},
                     chartArea: { width: '60%' },
-                    vAxis: {title: 'Self Sufficiency',format: '#\'%\'',titleTextStyle: {
-                        color: "#000",
-                        fontName: "Roboto",
+                    vAxis: {title: 'Self Sufficiency',format: '#\'%\'',textStyle:{color: '#ff0000',fontSize : 10},titleTextStyle: {
                         fontSize: 14,
-                      
                         italic: false
                     }},
-                    hAxis: {title: 'System Combination',titleTextStyle: {
-                        color: "#000",
-                        fontName: "Roboto",
+                    hAxis: {title: 'System Combination',textStyle:{color: '#ff0000',fontSize : 10},titleTextStyle: {
                         fontSize: 14,                      
                         italic: false
                     }} ,
@@ -41,14 +37,6 @@ class Graph_1 extends  Component{
                         startup: true,
                         duration: 1500,
                         easing: 'out',
-                    },
-                    annotations: {
-                        alwaysOutside: true,
-                        textStyle: {
-                            fontSize:4
-                          
-                           
-                        }
                     }
                 }} 
             />
